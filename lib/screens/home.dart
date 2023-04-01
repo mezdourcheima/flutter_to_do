03b8146ff829d7b956a1ba1b27ca1081df5f1b11
cheima mezdour <jc_mezdour@esi.dz>
2023-04-01 23:58:49 +0100

    feat : add input for to do items

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -51,14 +51,24 @@ class Home extends StatelessWidget {
                 Expanded(
                   child: Container(
                     margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
                     decoration: BoxDecoration(
-                      color : Colors.white,
-                      boxShadow: const  [BoxShadow(
-                        color: Colors.grey,
-                        offset: (0.0,0.0),
-                        blurRadius: 10.0,
-                        spreadRadius: 0.0,
-                      ),]
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Add new To do item',
+                        border: InputBorder.none,
+                      ),
                     ),
                   ),
                 )
